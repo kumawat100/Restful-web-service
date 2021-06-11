825d4274a77ac59b1033edc80ede564f99c66eb4
kumawat100 <kumawat10@gmail.com>
2021-06-11 13:44:33 +0200

    Internationalization

--- src/main/java/com/webServices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/com/webServices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.webServices.restfulwebservices.helloWorld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,20 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
-	//@GetMapping(path="/hello-world")
-	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+
+	@Autowired
+	private MessageSource messageSource;
+
+	// @GetMapping(path="/hello-world")
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	public String getHelloWorld() {
 		return "Hello World New";
 	}
-	
-	@GetMapping(path="/hello-world-bean")
+
+	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean getHelloWorldBean() {
 		return new HelloWorldBean("Hello World Bean");
 	}
-	
-	@GetMapping(path="/hello-world-bean/{name}")
+
+	@GetMapping(path = "/hello-world-bean/{name}")
 	public HelloWorldBean getHelloWorldBean(@PathVariable String name) {
 		return new HelloWorldBean("Hello World Bean " + name);
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String getHelloWorldInternationalized(
+			//@RequestHeader(name = "Accept-Language", required = false) Locale locale
+			) {
+		// en = Good Morning
+		// fn = Bonjour
+		// nl = Goede Morgen
+
+		//return messageSource.getMessage("good.morning.message", null, "Default Good Morning", locale);
+		return messageSource.getMessage("good.morning.message", null, "Default Good Morning", LocaleContextHolder.getLocale());
 	}
 }
